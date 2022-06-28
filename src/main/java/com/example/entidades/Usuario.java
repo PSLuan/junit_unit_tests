06dde8d7c2353dc5d564507ef4948493989b44bc
Luan Pereira <luanpereira.main@gmail.com>
2022-06-27 22:40:37 -0300

    Unit tests without junit

---
+++ src/main/java/com/example/entidades/Usuario.java
@@ -0,0 +1,21 @@
+package com.example.entidades;
+
+public class Usuario {
+
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/com/example/servicos/LocacaoService.java
@@ -0,0 +1,46 @@
+package com.example.servicos;
+
+import com.example.entidades.Filme;
+import com.example.entidades.Locacao;
+import com.example.entidades.Usuario;
+import com.example.utils.DataUtils;
+
+import java.util.Date;
+
+public class LocacaoService {
+
+    public Locacao alugarFilme(Usuario usuario, Filme filme) {
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        locacao.setValor(filme.getPrecoLocacao());
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = DataUtils.adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+
+    public static void main(String[] args) {
+        //cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        //acao
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificacao
+        System.out.println(locacao.getValor() == 5.0);
+        System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+    }
+
+}
